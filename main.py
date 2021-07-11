eceee0575eed33eb27c395a91cef4c0fb0770b90
sparsh2 <sarodesparsh@gmail.com>
2021-07-11 18:25:01 +0530

    face detection

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import cv2
 
-vid = cv2.VideoCapture(0)
-  
-while(True):
-    ret, frame = vid.read()
-    cv2.imshow('frame', frame)
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# To capture video from webcam. 
+cap = cv2.VideoCapture(0)
+# To use a video file as input 
+# cap = cv2.VideoCapture('filename.mp4')
+
+while True:
+    _, img = cap.read()
+    img2 = img
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    cv2.imshow('img', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-vid.release()
-cv2.destroyAllWindows()
+cap.release()
